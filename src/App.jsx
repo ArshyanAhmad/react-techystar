0acea518bb83461294c29c8786f4148252c14f17
Arsh <arshahmad235@gmail.com>
2024-11-03 10:20:47 +0530

    Contact form added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Footer from "./components/Footer";
+import Contact from "./components/Contact";
 
 import "./styles/App.scss";
 import "./styles/Header.scss";
 import "./styles/Home.scss";
+import "./styles/Footer.scss";
+import "./styles/contact.scss";
 
 function App() {
   return (
@@ -12,7 +16,9 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
